1515ed1520cbdfdbe9e8b37031ed6b1c3627a9ec
juthakarn <5635512142@email.psu.ac.th>
2018-02-04 00:18:16 +0700

    Book.java created

---
+++ Book.java
@@ -0,0 +1,41 @@
+public class Book{
+
+private String title
+private int pagenumber
+private String auther
+
+public Book (String title,int pagenumber,String auther){
+
+	this.title = title;
+	this.pagenumber = pagenumber;
+	this.auther = auther;
+
+}
+
+public String gettitle(){
+
+	return title;
+}
+public int getpagenumber(){
+
+	return pagenumber;
+}
+public String getauther(){
+
+	return auther;
+}
+
+public void settitle(String title){
+
+	this.title = title;
+}
+public void setpagenumber(int pagenumber){
+
+	this.pagenumber - pagenumber;
+}
+public void setauther(String auther){
+
+	this.auther = auther;
+}
+k
+}
