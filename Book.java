85fba2ad38a37617206d8d896ce453cbd2e6433b
juthakarn <5635512142@email.psu.ac.th>
2018-02-04 00:32:16 +0700

    samebook

--- Book.java
+++ Book.java
@@ -13,29 +13,38 @@ public Book (String title,int pagenumber,String auther){
 }
 
 public String gettitle(){
-
 	return title;
 }
 public int getpagenumber(){
-
 	return pagenumber;
 }
 public String getauther(){
-
 	return auther;
 }
 
 public void settitle(String title){
-
 	this.title = title;
 }
 public void setpagenumber(int pagenumber){
-
 	this.pagenumber - pagenumber;
 }
 public void setauther(String auther){
-
 	this.auther = auther;
 }
-k
+
+public void showBook(){
+	System.out.println(title);
+	System.out.println(pagenumber);
+	System.out.println(auther);
+}
+
+public boolean samebook(String title,int pagenumber,String auther){
+	if (this.title == title&&this.pagenumber == pagenumber&&this.auther == auther){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
 }
